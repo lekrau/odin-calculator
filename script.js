1eeec479c6d722b4a4069bbd717b3037ba2bd592
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-27 11:34:47 +0200

    Implement calculator input and evaluation

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-// Exercise: TBD
-
-// Problem Description in My Own Words
-// TBD
-
-// Input: TBD
-// Output: TBD
-
-// Assumptions
-// - TBD
-
-// Test Cases (2 - 4)
-// Normal case:
-// FUNCTION(ARGUMENTS) -> RESULT
-// Edge / boundary case:
-// FUNCTION(ARGUMENTS) -> RESULT
-// OPTIONAL Case related to assumptions:
-// FUNCTION(ARGUMENTS) -> RESULT
-// OPTIONAL Out of scope:
-// FUNCTION(ARGUMENTS) -> no valid result
-
-// Rough Plan / Subproblems
-// - TBD
-// Next Step (Smallest Subproblem): TBD
-
-// Pseudocode for the Next Step
-// TBD
-
-// Implementation
 "use strict";
 
+// FUNCTIONS
 const add = (summand1, summand2) => {
     const sum = summand1 + summand2;
     return sum;
@@ -50,23 +22,19 @@ const divide = (dividend, divisor) => {
 }
 
 const operate = (operator, number1, number2) => {
+    const value1 = +number1;
+    const value2 = +number2;
     switch (operator) {
-        case "add":
-            return (add(number1, number2));
-            break;
-        case "subtract":
-            return (subtract(number1, number2));
-            break;
-        case "multiply":
-            return (multiply(number1, number2));
-            break;
-        case "divide":
-            return (divide(number1, number2));
-            break;
-    
+        case "+":
+            return (add(value1, value2));
+        case "-":
+            return (subtract(value1, value2));
+        case "*":
+            return (multiply(value1, value2));
+        case "/":
+            return (divide(value1, value2));
         default:
-            return operator(number1, number2);
-            break;
+            return operator(value1, value2);
     }
 }
 
@@ -76,25 +44,45 @@ const processClicks = event => {
     const targetValue = target.textContent;
     switch (targetClass) {
         case "digit":
-            number1 += targetValue;
-            updateDisplay(number1);
+            if (operator === "") {
+                number1 += targetValue;
+                updateDisplay(number1);
+            } else {
+                number2 += targetValue;
+                updateDisplay(number2);
+            }
             break;
-    
+        case "operator":
+            operator = targetValue;
+            break;
+        case "equal-sign":
+            updateDisplay(operate(operator, number1, number2));
+            number1 = "";
+            number2 = "";
+            operator = "";
+            break;
+        case "clear":
+            break;
         default:
+            console.log(target);
             break;
     }
 }
 
-const updateDisplay = number => {
+const updateDisplay = value => {
     const display = document.querySelector(".display");
-    display.textContent = number;
+    display.textContent = value;
 }
 
-document.addEventListener("click", processClicks);
+// DECLARATIONS
 
 let number1 = "";
 let number2 = "";
 let operator = "";
+
+// MAIN
+
+document.addEventListener("click", processClicks);
 
 // Short Debug Log
 // What went wrong, if anything?
